de278538e18a063884254abd11415be3938b2e83
Vila71 <lolivila71@gmail.com>
2024-01-11 15:06:37 +0100

    topten card API data received

---
+++ src/components/atoms/cardTopten/CardTopten.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../cardTopten/cardTopten.css";
+
+const CardTopten = (props) => {
+  return (
+    <div className="countryContainer">
+      <div>
+        <div className="toptenFlag" >
+            <img src={props.toptenFlagValue} />
+        </div>
+        <div className="toptenCountry">
+            <p>{props.toptenNameCountryValue}</p>
+        </div>
+      </div>
+      <div className="toptenConfirmedCase">
+        <p>{props.toptenConfirmedCaseCountryValue}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CardTopten;
